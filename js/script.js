3e7773acb405c97c2ed67eac9eb4ddd8d8c3b5e5
viniciosragazzi <viniciosragazzzi@gmail.com>
2020-11-25 18:56:57 -0300

    Alterei o bg de gif para video com audio

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let lg = document.querySelector(".login");
 let regi = document.querySelector(".registro");
 let back = document.querySelector(".back");
 let voltar = document.querySelector(".voltar");
+let vid = document.querySelector(".vd");
 function regis(){
    
     regi.style.marginLeft = '0px';
@@ -30,6 +31,7 @@ function entrar(){
     voltar.style.display = 'flex'
     regi.style.Marginleft = '800px'
     back.style.width = '100%';
+    vid.style.right = '0';
     
 }
 function vt(){
@@ -37,4 +39,5 @@ function vt(){
     voltar.style.display = 'none'
     regi.style.Marginleft = '800px'
     back.style.width = '70%';
+    vid.style.right = '200px';
 }
