dfd2df68f66cbf4fd000a582e1de946c9f7ec9f5
viniciosragazzi <viniciosragazzzi@gmail.com>
2020-11-28 23:37:32 -0300

    Tela do Game Adicionada

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ let back = document.querySelector(".back");
 let voltar = document.querySelector(".voltar");
 let vid = document.querySelector(".vd");
 let iniciar = document.querySelector(".iniciar-bg");
+let textoIniciar = document.querySelector(".text")
+let telaGame = document.querySelector(".tela-game")
+let corpo = document.querySelector(".corpo")
+let social =  document.querySelector(".area-social")
+let socialL =  document.querySelector(".social-lateral")
+let ptexto =  document.querySelector(".pp")
+let picon =  document.querySelector(".pp2")
+let icos =  document.querySelector(".icos")
+let prin =  document.querySelector(".area-usuario")
+let chat = document.querySelector(".chat")
+let fechar = document.querySelector(".fechar")
+let down = document.querySelector(".down")
+let nc = document.querySelector(".nome-chat")
+let con = document.querySelector(".config")
+let arealogo2 = document.querySelector(".area-logo2")
+let arealogom = document.querySelector(".area-logom")
+
+
 function regis(){
    
     regi.style.marginLeft = '0px';
@@ -17,7 +35,7 @@ function regis(){
 }
 function logar(){
     
-    regi.style.marginLeft = '400px';
+    regi.style.marginLeft = '600px';
     regi.style.width = '600px';
     regi.style.height = '600px';
     back.style.width = '70%';
@@ -48,4 +66,63 @@ function vt(){
     regi.style.display = 'flex'
     
     
+}
+
+function clickJogar(){
+    lg.style.display = "none";
+    regi.style.display = "none";
+    back.style.display = "none";
+    voltar.style.display = "none";
+    iniciar.style.display = "none";
+    textoIniciar.style.display = "none";
+    telaGame.style.display = "flex"
+    vid.style.filter = "blur(0)";
+    corpo.style.backgroundColor = '#4F0F63';
+}
+
+function clicksocial(){
+ 
+    socialL.style.transform = "translateY(40px)";
+    socialL.style.width = "200px"; 
+    socialL.style.height = "400px"; 
+    ptexto.style.display = "none";
+    picon.style.display = "flex"
+    socialL.style.display = "flex"
+}
+
+function fecharsocial(){
+    socialL.style.transform = "translateY(0)";
+    socialL.style.width = "100px"; 
+    socialL.style.height = "40px"; 
+    ptexto.style.display = "flex";
+    picon.style.display = "none"
+    icos.style.paddingTop="12px"
+    socialL.style.display = "none"
+   
+}
+
+function clickChat(){
+    chat.style.height = "300px"
+    down.style.display = "none"
+    fechar.style.display = "flex"
+    nc.style.borderBottom = "2px solid rgb(114 10 155)"
+    
+}
+function fecharChat(){
+    chat.style.height = "20px"
+    down.style.display = "flex"
+    fechar.style.display = "none"
+    nc.style.borderBottom = "none"
+}
+function clickLogo(){
+    con.style.display = "flex"
+    con.style.transform = "translateY(90px)"
+    arealogom.style.display = "none"
+    arealogo2.style.display = "flex"
+}
+function fecharConfig(){
+    con.style.display = "none"
+    con.style.transform = "translateY(-90px)"
+    arealogom.style.display = "flex"
+    arealogo2.style.display = "none"
 }
